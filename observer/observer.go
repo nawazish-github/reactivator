17867186a52ffb250430701144148de14cdee508
mnawaz <mnawaz@thoughtworks.com>
2018-06-17 16:09:07 +0530

    refactor observer to use handlers package and provide default observer

--- observer/observer.go
+++ observer/observer.go
@@ -1,15 +1,55 @@
 package observer
 
-type Observer struct {
-	OnNext     OnNext
-	OnError    OnError
-	OnComplete OnComplete
-}
+import (
+	"log"
 
-type OnNext func(interface{})
-type OnError func(error)
-type OnComplete func()
+	"github.com/nawazish-github/reactivator/handlers"
+)
+
+type Observer struct {
+	OnNext     handlers.OnNext
+	OnError    handlers.OnError
+	OnComplete handlers.OnComplete
+}
 
 func (observer *Observer) Dispose() {
 
+}
+
+func New(eventHandlers ...handlers.EventHandler) Observer {
+	observer := DefaultObserver
+	if len(eventHandlers) > 0 {
+		for _, handler := range eventHandlers {
+			switch tipe := handler.(type) {
+			case handlers.OnNext:
+				observer.OnNext = tipe
+			case handlers.OnComplete:
+				observer.OnComplete = tipe
+			case handlers.OnError:
+				observer.OnError = tipe
+			}
+		}
+	}
+	return observer
+}
+
+func (observer Observer) Handle(seq interface{}) {
+	switch tipe := seq.(type) {
+	case error:
+		observer.OnError(tipe)
+	default:
+		observer.OnNext(tipe)
+	}
+}
+
+var DefaultObserver = Observer{
+	OnNext: func(seq interface{}) {
+		log.Println(seq)
+	},
+	OnComplete: func() {
+		log.Println("OnComplete signalled")
+	},
+	OnError: func(e error) {
+		log.Fatal(e)
+	},
 }
