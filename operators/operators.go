9d977354a517cb038fc7248701f73d8b3854e712
mnawaz <mnawaz@thoughtworks.com>
2018-06-21 00:17:16 +0530

    add Range operator

--- operators/operators.go
+++ operators/operators.go
@@ -15,5 +15,15 @@ func Range(start int, length int) (observable.Observable, error) {
 	if length == 0 {
 		return nil, nil
 	}
-	return nil, nil
+
+	observable := make(chan interface{})
+	go func() {
+		defer close(observable)
+		counter := 0
+		for i := start; counter < length; counter++ {
+			observable <- i
+			i++
+		}
+	}()
+	return observable, nil
 }
