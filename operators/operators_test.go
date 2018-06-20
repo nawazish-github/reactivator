c8c9e21e071b4b5d5e058b484cc5cfd479c4d9ad
mnawaz <mnawaz@thoughtworks.com>
2018-06-21 00:16:20 +0530

    add tests for Range operator

--- operators/operators_test.go
+++ operators/operators_test.go
@@ -23,3 +23,36 @@ func TestRange_ShouldReturnNilObserverIfLengthIsZero(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestRange_ShouldReturnValidObservable(t *testing.T) {
+	start := 0
+	length := 4
+
+	observable, err := Range(start, length)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if observable == nil {
+		t.Fail()
+	}
+
+	var si []int
+	for value := range observable {
+		if v, ok := value.(int); ok {
+			si = append(si, v)
+		}
+	}
+	if len(si) != 4 {
+		t.Fail()
+	}
+	counter := start
+
+	for i := range si {
+		if counter != i {
+			t.Fail()
+		}
+		counter++
+	}
+}
