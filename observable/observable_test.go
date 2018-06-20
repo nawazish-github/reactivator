483042f8c9f074624847423b0be60ccc9b821ce5
mnawaz <mnawaz@thoughtworks.com>
2018-06-20 19:40:03 +0530

    add test to verify OnComplete Handler

--- observable/observable_test.go
+++ observable/observable_test.go
@@ -15,7 +15,7 @@ func TestFromOperatorWithUniformDataAndDefaultOnNextObserver(t *testing.T) {
 		t.Errorf("Could not create observable from Uniform data slice ")
 	}
 	data := []interface{}{}
-	observer := observer.New(handlers.OnNext(func(seq interface{}) {
+	observer := observer.RegisterHandlers(handlers.OnNext(func(seq interface{}) {
 		switch tipe := seq.(type) {
 		case int:
 			data = append(data, tipe)
@@ -36,11 +36,31 @@ func TestFromOperatorWithUniformDataAndDefaultOnCompleteObserver(t *testing.T) {
 		t.Errorf("Could not create observable from Uniform data slice ")
 	}
 	data := []interface{}{}
-	observer := observer.New(handlers.OnComplete(func() {
+	observer := observer.RegisterHandlers(handlers.OnComplete(func() {
 		data = append(data, 1) //adding one to data slice signifying that OnComplete was called
 	}))
 	subChan := observable.Subscribe(observer)
 	<-subChan
 
 	assert.Len(t, data, 1, "Length of sequence emitted did not match")
+}
+
+func TestFromOperator_ShouldCallOnlyOnCompleteHandler(t *testing.T) {
+	testTable := []interface{}{}
+	observable, err := From(testTable)
+	if err != nil {
+		t.Errorf("Could not create observable from Uniform data slice ")
+	}
+
+	onCompleteFlag := false
+	ob := observer.RegisterHandlers(handlers.OnComplete(func() { onCompleteFlag = true }),
+		handlers.OnNext(func(seq interface{}) { t.Fail() }),
+		handlers.OnError(func(e error) { t.Fail() }),
+	)
+
+	<-observable.Subscribe(ob)
+
+	if !onCompleteFlag {
+		t.Fail()
+	}
 }
