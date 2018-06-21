0a161abe3f6cc3edff6df52b6c29207580e60fcb
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 01:06:59 +0530

    refactor observable by pulling out operators into separate files

--- observable/observable.go
+++ observable/observable.go
@@ -3,33 +3,12 @@
 package observable
 
 import (
-	"errors"
 	"log"
 
 	"github.com/nawazish-github/reactivator/observer"
 )
 
 type Observable <-chan interface{}
-
-func From(data interface{}) (Observable, error) {
-	switch t := data.(type) {
-	case []interface{}:
-		obsChan := make(chan interface{}, len(t))
-		go func() {
-			defer close(obsChan)
-			for _, val := range t {
-				obsChan <- val
-			}
-		}()
-		return obsChan, nil
-	case <-chan interface{}:
-		return Observable(t), nil
-	case chan interface{}:
-		return Observable(t), nil
-	default:
-		return nil, errors.New("Invalid type. Could not create Observable from data!")
-	}
-}
 
 func (observable Observable) Subscribe(observer observer.Observer) chan struct{} {
 	subscriptionChannel := make(chan struct{})
