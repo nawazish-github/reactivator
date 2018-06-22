3506272a704b3cd3653ec829f451e893b48bf089
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 17:43:25 +0530

    Add test case on Filter operator when filter function is nil return same observable on which Filter was called

--- observable/operator-filter_test.go
+++ observable/operator-filter_test.go
@@ -14,3 +14,35 @@ func TestFilterOperator_ShouldReturnErrorWhenObservableOnWhichFilterIsCalledIsNi
 		t.Fail()
 	}
 }
+
+func TestFilterOperator_ShouldReturnSameObservableWhenFilterFunctionIsNil(t *testing.T) {
+
+	expectation := []int{1, 2, 3, 4, 5}
+
+	obsChan := make(chan interface{}, 5)
+	for i := 1; i <= 5; i++ {
+		obsChan <- i
+	}
+	close(obsChan)
+	testObservable := Observable(obsChan)
+
+	obs, err := testObservable.Filter(nil)
+	if err != nil {
+		t.Fail()
+	}
+	var result []int
+	for val := range obs {
+		x := val.(int)
+		result = append(result, x)
+	}
+
+	if len(result) != len(expectation) {
+		t.Fail()
+	}
+
+	for index := range result {
+		if result[index] != expectation[index] {
+			t.Fail()
+		}
+	}
+}
