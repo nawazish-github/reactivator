318d5db9099a9f23e0fd236357831ae43195466a
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 17:44:21 +0530

    Add functionality on Filter Operator to return same Observer on which Filter was called if filter function was nil

--- observable/operator-filter.go
+++ observable/operator-filter.go
@@ -11,5 +11,9 @@ func (observable Observable) Filter(fx filterFunc) (Observable, error) {
 	if observable == nil {
 		return nil, errors.New("Nil Observable: Filter operator called on Nil Observable")
 	}
+
+	if fx == nil {
+		return observable, nil
+	}
 	return nil, nil
 }
