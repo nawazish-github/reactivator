98a9794475216ceccb2f3cc16425cc4861d7b6ca
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 16:15:00 +0530

    Add implementation to apply mapper function on given Observable sequence

--- observable/operator-map.go
+++ observable/operator-map.go
@@ -1,11 +1,27 @@
 package observable
 
-type mapFunction func(x ...interface{}) interface{}
+import "errors"
+
+type mapFunction func(x interface{}) interface{}
 
 func (observable Observable) Map(fx mapFunction) (Observable, error) {
+
+	if observable == nil {
+		return nil, errors.New("Nil observable: Map operator called on nil observable")
+	}
 
 	if fx == nil {
 		return observable, nil
 	}
-	return nil, nil
+
+	mapChan := make(chan interface{})
+	go func() {
+		for val := range observable {
+			y := fx(val)
+			mapChan <- y
+		}
+		close(mapChan)
+	}()
+
+	return Observable(mapChan), nil
 }
