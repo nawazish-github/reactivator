6d64b18f92593644b62033db8d0694fa8f11b1c8
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 14:16:47 +0530

    Add tests for Interval operator

---
+++ observable/operator-interval_test.go
@@ -0,0 +1,41 @@
+package observable
+
+import (
+	"testing"
+	"time"
+)
+
+func TestIntervalOperator_ShouldReturnValidIntervalObserver(t *testing.T) {
+	var testObservable Observable
+	duration := time.Duration(time.Second)
+
+	observable, err := testObservable.Interval(duration)
+	if err != nil {
+		t.Fail()
+	}
+	if observable == nil {
+		t.Fail()
+	}
+
+	doneChan := make(chan struct{})
+	go func() {
+		time.Sleep(5 * time.Second)
+		doneChan <- struct{}{}
+	}()
+
+	counter := 0
+	var sliceOfInt []int
+ForLoop:
+	for {
+		select {
+		case <-doneChan:
+			break ForLoop
+		case <-observable:
+			sliceOfInt = append(sliceOfInt, counter)
+			counter++
+		}
+	}
+	if len(sliceOfInt) != 5 {
+		t.Fail()
+	}
+}
