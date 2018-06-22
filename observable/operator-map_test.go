3980f2928843e7ffd72627df0740da77fae2549c
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 16:14:18 +0530

    Add test case to apply mapper function on given Observable

--- observable/operator-map_test.go
+++ observable/operator-map_test.go
@@ -40,3 +40,53 @@ func TestMapOperator_ShouldReturnTheSameObservableIfMapperFunctionIsNil(t *testi
 		}
 	}
 }
+
+func TestMapOperator_ShouldReturnErrorIfObservableOnWhichMapOperatorIsCalledIsNil(t *testing.T) {
+	var observable Observable
+	mapper := func(x interface{}) interface{} {
+		return x
+	}
+	_, err := observable.Map(mapper)
+
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestMapOperator_ShouldReturnValidObservableAfterApplyingMapperFunction(t *testing.T) {
+	obsChan := make(chan interface{}, 5)
+	for i := 1; i <= 5; i++ {
+		obsChan <- i
+	}
+	close(obsChan)
+	testObservable := Observable(obsChan)
+	expectation := []int{3, 4, 5, 6, 7}
+	var result []int
+	obs, err := testObservable.Map(func(x interface{}) interface{} {
+		y := x.(int)
+		return y + 2
+	})
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if obs == nil {
+		t.Fail()
+	}
+
+	for val := range obs {
+		x := val.(int)
+		result = append(result, x)
+	}
+
+	if len(expectation) != len(result) {
+		t.Fail()
+	}
+
+	for index := range result {
+		if expectation[index] != result[index] {
+			t.Fail()
+		}
+	}
+}
