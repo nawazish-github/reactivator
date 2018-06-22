500ccde8ea08b28fa78c327a9575a52626bb9502
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 17:33:02 +0530

    Filter operator with check if Obseravble is Nil return Nil

---
+++ observable/operator-filter.go
@@ -0,0 +1,15 @@
+package observable
+
+import (
+	"errors"
+)
+
+type filterFunc func(x interface{}) bool
+
+func (observable Observable) Filter(fx filterFunc) (Observable, error) {
+
+	if observable == nil {
+		return nil, errors.New("Nil Observable: Filter operator called on Nil Observable")
+	}
+	return nil, nil
+}
