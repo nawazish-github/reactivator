28c40b417da39aeee76f87ba3340217b6901dca0
mnawaz <mnawaz@thoughtworks.com>
2018-06-22 18:01:55 +0530

    Add test case to Filter operator to apply  filter function to each Observable sequence

--- observable/operator-filter_test.go
+++ observable/operator-filter_test.go
@@ -46,3 +46,49 @@ func TestFilterOperator_ShouldReturnSameObservableWhenFilterFunctionIsNil(t *tes
 		}
 	}
 }
+
+func TestFilterOperator_ShouldApplyFilterFunctionAndReturnValidObservable(t *testing.T) {
+	expectation := []int{1, 3, 5}
+
+	obsChan := make(chan interface{}, 5)
+	for i := 1; i <= 5; i++ {
+		obsChan <- i
+	}
+	close(obsChan)
+	testObservable := Observable(obsChan)
+
+	obs, err := testObservable.Filter(func(x interface{}) bool {
+		y := x.(int)
+
+		if y%2 != 0 {
+			return true
+		} else {
+			return false
+		}
+
+	})
+	if err != nil {
+		t.Fail()
+	}
+
+	if obs == nil {
+		t.Fail()
+	}
+
+	var result []int
+
+	for val := range obs {
+		y := val.(int)
+		result = append(result, y)
+	}
+
+	if len(expectation) != len(result) {
+		t.Fail()
+	}
+
+	for index := range result {
+		if result[index] != expectation[index] {
+			t.Fail()
+		}
+	}
+}
