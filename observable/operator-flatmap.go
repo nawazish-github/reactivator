9bd4186a17608694b27716f9489b3064c8300ed7
mnawaz <mnawaz@thoughtworks.com>
2018-06-25 22:41:38 +0530

    FlatMap operator to return valid Observable and corresponding test case

--- observable/operator-flatmap.go
+++ observable/operator-flatmap.go
@@ -1,10 +1,38 @@
 package observable
 
-func (observable Observable) FlatMap(soOb []Observable) Observable {
+import (
+	"sync"
+)
 
-	if len(soOb) == 0 {
-		panic("Illegal Argument: len of input slice to FlatMap is zero")
+func (observables Observable) FlatMap() Observable {
+
+	if observables == nil {
+		panic("Nil Pointer: FlatMap called on Nil Observable")
 	}
-	var obs Observable
-	return obs
+	var wg sync.WaitGroup
+	var sliceOfObservables []Observable
+	obs := make(chan interface{})
+
+	for ob := range observables {
+		x := ob.(Observable)
+		sliceOfObservables = append(sliceOfObservables, x)
+	}
+
+	wg.Add(len(sliceOfObservables))
+
+	go func() {
+		defer close(obs)
+		wg.Wait()
+	}()
+
+	for _, observable := range sliceOfObservables {
+		go func(observable Observable) {
+			defer wg.Done()
+			for val := range observable {
+				obs <- val
+			}
+		}(observable)
+	}
+
+	return Observable(obs)
 }
--- observable/operator-flatmap_test.go
+++ observable/operator-flatmap_test.go
@@ -14,3 +14,37 @@ func TestFlatMapOperator_ShouldPanicIfInputSliceLenIsZero(t *testing.T) {
 
 	testObservable.FlatMap()
 }
+
+func TestFlatMapOperator_ShouldReturnValidFlatMapObservable(t *testing.T) {
+	var observable Observable
+	var slice []int
+	expectation := []int{1, 2, 3, 4, 5, 6}
+	obs1, _ := observable.From([]interface{}{1, 2, 3})
+	obs2, _ := observable.From([]interface{}{4, 5, 6})
+	soOb := []interface{}{obs1, obs2}
+	fromOb, _ := observable.From(soOb)
+	flatMapObser := fromOb.FlatMap()
+	for val := range flatMapObser {
+		x := val.(int)
+		slice = append(slice, x)
+	}
+
+	if len(slice) != len(expectation) {
+		t.Fail()
+	}
+
+	found := false
+	for _, val := range slice {
+		found = false
+		for _, expectedData := range expectation {
+			if expectedData == val {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Fail()
+			break
+		}
+	}
+}
