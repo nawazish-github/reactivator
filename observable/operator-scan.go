4e23898de5a29d90741d5234ff6c3d87d7b1d146
mnawaz <mnawaz@thoughtworks.com>
2018-07-01 17:17:29 +0530

    Add core Scan functionality

--- observable/operator-scan.go
+++ observable/operator-scan.go
@@ -1,8 +1,8 @@
 package observable
 
-type ScanFunc func(x interface{}) interface{}
+type ScanFunc func(x, y interface{}) interface{}
 
-func (observable Observable) Scan(fx ScanFunc) Observable {
+func (observable Observable) Scan(fx ScanFunc, zeroValue interface{}) Observable {
 	if observable == nil {
 		panic("Illegal Argument: Scan called on nil Observable")
 	}
@@ -10,5 +10,15 @@ func (observable Observable) Scan(fx ScanFunc) Observable {
 	if fx == nil {
 		return observable
 	}
-	return nil
+
+	scanChan := make(chan interface{})
+	go func() {
+		var acc interface{} = zeroValue
+		defer close(scanChan)
+		for val := range observable {
+			acc = fx(val, acc)
+			scanChan <- acc
+		}
+	}()
+	return Observable(scanChan)
 }
