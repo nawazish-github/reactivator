03a9ea5a5c13a48fd39a88ae9ced3677b24699b4
mnawaz <mnawaz@thoughtworks.com>
2018-07-01 15:39:47 +0530

    Add test case to verify Scan return same Observable when ScanFunc is nil

--- observable/operator-scan_test.go
+++ observable/operator-scan_test.go
@@ -13,3 +13,13 @@ func TestScanOperator_ShouldPanicIfObservableOnWhichScanIsCalledIsNil(t *testing
 	var testObservable Observable
 	testObservable.Scan(nil)
 }
+
+func TestScanOperator_ShouldReturnSameObservableIfScanFuncIsNil(t *testing.T) {
+
+	var testObservable Observable
+	obs, _ := testObservable.From([]interface{}{1, 2, 3})
+	o := obs.Scan(nil)
+	if !CompareIntegerSlices(o, []int{1, 2, 3}) {
+		t.Errorf("Scan did not return the same Observable when input function was nil")
+	}
+}
