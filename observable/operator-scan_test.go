765ad99c7653dfdcb59cf626339e8fcb40de2a2c
mnawaz <mnawaz@thoughtworks.com>
2018-07-01 17:17:01 +0530

    Add test case to verify core Scan functionality is correct

--- observable/operator-scan_test.go
+++ observable/operator-scan_test.go
@@ -11,15 +11,31 @@ func TestScanOperator_ShouldPanicIfObservableOnWhichScanIsCalledIsNil(t *testing
 		}
 	}()
 	var testObservable Observable
-	testObservable.Scan(nil)
+	testObservable.Scan(nil, nil)
 }
 
 func TestScanOperator_ShouldReturnSameObservableIfScanFuncIsNil(t *testing.T) {
 
 	var testObservable Observable
 	obs, _ := testObservable.From([]interface{}{1, 2, 3})
-	o := obs.Scan(nil)
-	if !CompareIntegerSlices(o, []int{1, 2, 3}) {
+	o := obs.Scan(nil, nil)
+	if !CompareIntegerSlices(o, []interface{}{1, 2, 3}) {
 		t.Errorf("Scan did not return the same Observable when input function was nil")
+	}
+}
+
+func TestScanOperator_ShouldReturnValidObservable(t *testing.T) {
+	var scanFunc ScanFunc = func(x, y interface{}) interface{} {
+		xInt := x.(int)
+		yInt := y.(int)
+		return interface{}((xInt + yInt))
+	}
+
+	var testObservable Observable
+	obs, _ := testObservable.From([]interface{}{4, 5, 6})
+
+	o := obs.Scan(scanFunc, 0)
+	if !CompareIntegerSlices(o, []interface{}{4, 9, 15}) {
+		t.Errorf("Scan did not apply ScanFunc correctly")
 	}
 }
